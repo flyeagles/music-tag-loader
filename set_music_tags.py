94d2d05cce2316618b3477593acf437886af34d4
Yi Luo <yluo@microstrategy.com>
2023-11-30 10:50:37 +0800

    add album artist field for tags setter

--- set_music_tags.py
+++ set_music_tags.py
@@ -10,7 +10,7 @@ from mutagen.mp3 import MP3
 from mutagen.wave import WAVE
 from mutagen.dsdiff import DSDIFF
 from mutagen.dsf import DSF
-from mutagen.id3 import ID3, TIT2, TALB, TRCK, TPE1, TDRC
+from mutagen.id3 import ID3, TIT2, TALB, TRCK, TPE1, TDRC, TPE2
 from mutagen.easyid3 import EasyID3
 
 
@@ -19,43 +19,49 @@ from mutagen.easyid3 import EasyID3
 logger = logging.getLogger('tag_loader')
 
 
-def set_wav_meta(filename, album, year, total, title_info, track_num):
+def set_wav_meta(filename, album, year, total, band, title_info, track_num):
     audio = WAVE(filename)
     audio.pprint()
     logger.debug(audio.tags)
-    set_mp3_metadata(audio, album, year, total, title_info, track_num)
+    set_mp3_metadata(audio, album, year, total, band, title_info, track_num)
 
 
-def set_flac_meta(filename, album, year, total, title_info):
+def set_flac_meta(filename, album, year, total, band, title_info):
     audio = FLAC(filename)
     logger.debug(audio.tags)
-    set_metadata(audio, album, year, total, title_info)
+    set_metadata(audio, album, year, total, band, title_info)
 
 
-def set_mp3_meta(filename, album, year, total, title_info):
+def set_mp3_meta(filename, album, year, total, band, title_info):
     audio = MP3(filename)
     logger.debug(audio.tags)
-    set_mp3_metadata(audio, album, year, total, title_info)
+    set_mp3_metadata(audio, album, year, total, band, title_info)
 
-def set_dff_meta(filename, album, year, total, title_info, track_num):
+
+def set_dff_meta(filename, album, year, total, band, title_info, track_num):
     audio = DSDIFF(filename)
     logger.debug(audio.tags)
-    set_mp3_metadata(audio, album, year, total, title_info, track_num)
+    set_mp3_metadata(audio, album, year, total, band, title_info, track_num)
 
-def set_dsf_meta(filename, album, year, total, title_info, track_num):
+
+def set_dsf_meta(filename, album, year, total, band, title_info, track_num):
     audio = DSF(filename)
     logger.debug(audio.tags)
-    set_mp3_metadata(audio, album, year, total, title_info, track_num)
-    
-def set_mp3_metadata(audio, album, year, total, title_info, track_num):
+    set_mp3_metadata(audio, album, year, total, band, title_info, track_num)
+
+
+def set_mp3_metadata(audio, album, year, total, band, title_info, track_num):
     '''
     {'TIT2': TIT2(encoding=<Encoding.UTF16: 1>, text=['2']),  # track title
     'TALB': TALB(encoding=<Encoding.UTF16: 1>, text=['3']),     # album title
     'TRCK': TRCK(encoding=<Encoding.LATIN1: 0>, text=['5']),    # track number
     'TPE1': TPE1(encoding=<Encoding.UTF16: 1>, text=['1'])}   #artist name
+    'TPE2': TPE2(encoding)   #band name
+    Please see "ID3 v2_4 Tags" section in https://www.exiftool.org/TagNames/ID3.html
     '''
     audio['TIT2'] = TIT2(encoding=3, text=[title_info[0]])
     audio['TPE1'] = TPE1(encoding=3, text=[title_info[1]])
+    audio['TPE2'] = TPE2(encoding=3, text=[band])
     audio['TRCK'] = TRCK(encoding=3, text=[f'{track_num}/{total}'])
     audio['TALB'] = TALB(encoding=3, text=[album])
     audio['TDRC'] = TDRC(encoding=3, text=[year])
@@ -143,20 +149,21 @@ music_func_map = {"flac": set_flac_meta,
                   }
 
 
-def handle_music_file(filename, album, year, total, title_info, track_num):
+def handle_music_file(filename, album, year, total, band, title_info, track_num):
     print(filename)
     surfix = filename.split('.')[-1].lower()
     (music_func_map[surfix])(filename, album,
-                             year, total, title_info, track_num)
+                             year, total, band, title_info, track_num)
 
 
 def is_music_file(filename):
     norm_filename = filename.lower()
     return norm_filename.endswith('flac') or norm_filename.endswith('ape') or norm_filename.endswith('mp3') or \
-        norm_filename.endswith('wav') or norm_filename.endswith('dff') or norm_filename.endswith('dsf')
+        norm_filename.endswith('wav') or norm_filename.endswith(
+            'dff') or norm_filename.endswith('dsf')
 
 
-def set_tags(baseroot, album, year, total, song_title_list):
+def set_tags(baseroot, album, year, total, band, song_title_list):
     print(baseroot)
 
     oldpath = os.getcwd()
@@ -166,7 +173,7 @@ def set_tags(baseroot, album, year, total, song_title_list):
     sorted_music_file_list = sorted(music_file_list)
 
     for idx, filename in enumerate(sorted_music_file_list):
-        handle_music_file(filename, album, year, total,
+        handle_music_file(filename, album, year, total, band,
                           song_title_list[idx], idx+1)
 
     os.chdir(oldpath)
@@ -178,6 +185,7 @@ def parse_song_file(filename):
     ALBUM|成都
     DATE|2017
     TOTAL|13
+    BAND|Someone
     成都|赵雷
     理想|赵雷
     '''
@@ -188,7 +196,7 @@ def parse_song_file(filename):
             if len(line) == 0:
                 continue
             song_title_list.append(line.split('|'))
-    return (song_title_list[0][1], song_title_list[1][1], song_title_list[2][1], song_title_list[3:])
+    return (song_title_list[0][1], song_title_list[1][1], song_title_list[2][1], song_title_list[3][1], song_title_list[4:])
 
 
 if __name__ == "__main__":
@@ -207,9 +215,9 @@ if __name__ == "__main__":
     else:
         logging.basicConfig(level=logging.ERROR)
 
-    album, year, total, song_title_list = parse_song_file(args.file)
-    print(album, year, total, song_title_list)
+    album, year, total, band, song_title_list = parse_song_file(args.file)
+    print(album, year, total, band, song_title_list)
 
-    count = set_tags(args.dir, album, year, total, song_title_list)
+    count = set_tags(args.dir, album, year, total, band, song_title_list)
 
     print(f'Processed {count} songs.')
